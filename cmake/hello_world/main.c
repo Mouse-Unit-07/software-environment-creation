c40d899e3c4d9ade7c20c885eef7a776892635b6
Ryuichi Lin <ryuno107lin@gmail.com>
2025-05-24 21:42:29 -0400

    feat: add cmake build directory

---
+++ cmake/hello_world/main.c
@@ -0,0 +1,16 @@
+/*
+ * hello_world.c
+ *
+ * Created: 5/24/2025 2:37:50 PM
+ * Author : ryuno
+ */ 
+
+#include <avr32/io.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("Hello World\r\n");
+}
+
